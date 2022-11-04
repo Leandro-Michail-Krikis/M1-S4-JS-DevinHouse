4b2800111f3679e8e2d09374de38a696fa66db3a
ACERLEANDRO\leo_d <leo1996.dasilva@gmail.com>
2022-11-03 22:36:32 -0300

    EX6

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 import Conta from "./Conta.js";
 try {
-  const form = document.getElementById("formulario");
+  let form = document.getElementById("formulario");
   form.addEventListener("submit", submit);
 } catch (error) {}
 
 try {
-  const formOperacoes = document.getElementById("formularioOperacoes");
+  let formOperacoes = document.getElementById("formularioOperacoes");
   formOperacoes.addEventListener("submit", submitOperacoes);
 } catch (error) {}
-const contas = [];
-
-function submitOperacoes(event) {
-  event.preventDefault();
-}
 
 function submit(event) {
+  const contas = getContas();
   event.preventDefault();
   if (event.target.senha.value != event.target.senhaVerificacao.value) {
     alert("as senhas nao conferem.");
+    event.target.senha.value = ""
+    event.target.senhaVerificacao.value = ""
+    return
   }
   contas.push(
     Conta.criarNovaConta(
@@ -28,13 +27,64 @@ function submit(event) {
     )
   );
 
-  salvaArray("contas", contas);
+  setContas(contas);
 
   alert("Conta criada com sucesso.");
 
   event.target.reset();
 }
 
-const salvaArray = (key, array) => {
-  localStorage.setItem(key, JSON.stringify(array));
+function submitOperacoes(event) {
+  event.preventDefault();
+
+  const contas = getContas();
+  const contaNumero = event.target.conta.value;
+  const senha = event.target.senha.value;
+
+  if (isNotUsuarioValido(contas, contaNumero, senha)) return;
+
+  switch (event.target.operacoes.value) {
+    case "saque":
+      saque();
+      break;
+
+    case "deposito":
+      deposito();
+      break;
+
+    case "saldo":
+      saldo();
+      break;
+  }
+}
+
+const isNotUsuarioValido = (arrayContas, contaNumero, senha) => {
+    arrayContas.forEach(obj => {
+      if(obj.nome == contaNumero && obj.senha == senha) return false;
+    });
+    return true
 };
+
+const setContas = (array) => {
+  localStorage.setItem("contas", JSON.stringify(array));
+};
+
+const getContas = () => {
+  try {
+    return JSON.parse(localStorage.getItem("contas"));
+  } catch (error) {
+    return [];
+  }
+};
+
+const saque = () => {
+
+}
+
+const deposito = () => {
+
+}
+
+const saldo = () => {
+  
+}
