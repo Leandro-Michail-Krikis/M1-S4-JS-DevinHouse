09c5db8aab373783030d1aab5f9fce7bdec5d969
ACERLEANDRO\leo_d <leo1996.dasilva@gmail.com>
2022-11-03 21:30:51 -0300

    EX5

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const contas = [];
-const form = document.getElementById("formulario");
-form.addEventListener("submit", submit);
 import Conta from "./Conta.js";
+try {
+  const form = document.getElementById("formulario");
+  form.addEventListener("submit", submit);
+} catch (error) {}
+
+try {
+  const formOperacoes = document.getElementById("formularioOperacoes");
+  formOperacoes.addEventListener("submit", submitOperacoes);
+} catch (error) {}
+const contas = [];
+
+function submitOperacoes(event) {
+  event.preventDefault();
+}
 
 function submit(event) {
   event.preventDefault();
   if (event.target.senha.value != event.target.senhaVerificacao.value) {
-    alert("as senhas nao conferem.")
+    alert("as senhas nao conferem.");
   }
   contas.push(
     Conta.criarNovaConta(
@@ -16,10 +27,10 @@ function submit(event) {
       event.target.senha.value
     )
   );
-  
+
   salvaArray("contas", contas);
 
-  alert("Conta criada com sucesso.")
+  alert("Conta criada com sucesso.");
 
   event.target.reset();
 }
